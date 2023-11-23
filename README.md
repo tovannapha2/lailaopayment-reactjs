51471c9652281cb24ec1b70df0b0afb26fb5f29a
TOxxTOxx <tovannapha2@gmail.com>
2023-11-23 15:36:46 +0700

    add readme

--- README.md
+++ README.md
@@ -1,70 +1,198 @@
-# Getting Started with Create React App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## ການເຊື່ອມຕໍ່Payment Gateway ດ້ວຍ ReactJS
 
-## Available Scripts
+ທ່ານສາມາດນຳໃຊ້ ReactJs ໃນການເຊື່ອມຕໍ່ກັບ Lailao Payment Gateway ຂອງພວກເຮົາ ກ່ອນອື່ນຫມົດແມ່ນເຮົາຕ້ອງສ້າງໂຄງການ Reactjs ເຊິ່ງລາຍລະອຽດເພີ່ມຕື່ມສາມາດເບິ່ງໄດ້ທີ https://legacy.reactjs.org/docs/create-a-new-react-app.html
 
-In the project directory, you can run:
 
-### `npm start`
+## ສ້າງ React App
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+ສ້າງ React App ເປັນສະພາບແວດລ້ອມທີ່ສະດວກສະບາຍສໍາລັບການຮຽນຮູ້ React, ແລະເປັນວິທີທີ່ດີທີ່ສຸດເພື່ອເລີ່ມຕົ້ນການສ້າງຄໍາຮ້ອງສະຫມັກຫນ້າດຽວໃຫມ່ໃນ React.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+ມັນຕັ້ງຄ່າສະພາບແວດລ້ອມການພັດທະນາຂອງທ່ານເພື່ອໃຫ້ທ່ານສາມາດນໍາໃຊ້ຄຸນສົມບັດ JavaScript ຫລ້າສຸດ, ສະຫນອງປະສົບການນັກພັດທະນາທີ່ດີ, ແລະເພີ່ມປະສິດທິພາບແອັບຯຂອງທ່ານໃນການຜະລິດ. ທ່ານຈະຕ້ອງມີ Node >= 14.0.0 ແລະ npm >= 5.6 ໃນເຄື່ອງຂອງເຈົ້າ. ເພື່ອສ້າງໂຄງການ, ດໍາເນີນການ:
 
-### `npm test`
+```bash
+npx create-react-app tutorial-reactjs
+cd tutorial-reactjs
+npm start
+```
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
-### `npm run build`
+ຕໍ້ໄປເຮົາສາມາດເພີ່ມ Library ຕ່າງໆເຊັ່ນ icon, ການສະແດງຮູບພາບ, ການໃຫ້ດາວ ແລະ tailwind ເພື່ອຊ່ວຍໃນການອອກແບບຫນ້າຕາຂອງເຮົາໃຫ້ສວຍງາມຂຶ້ນ ໂດຍພິມຄຳສັ່ງລຸ່ມນີ້
+```bash
+npm install react-icons react-image-gallery react-rater tailwindcss @headlessui/react  react-qrcode-logo 
+```
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+ເພື່ອຈຳລອງຫນ້າຕາລາຍລະອຽດສິນຄ້າທ່ານສາມາດໃຊ້ source code ລຸ່ມນີ້ເພື່ອຈຳລອງຂຶ້ນມາ ເຮົາຈະໄດ້ຮູບພາບສິນຄ້າ ແລະ ລາຍລະອຽດສິນຄ້າສະແດງຕົວຢ່າງຕາມດ້ານຂວາ
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+```JSX
+import './App.css';
+import { AiOutlineHeart } from "react-icons/ai";
+import { BiShoppingBag } from "react-icons/bi";
+import ReactImageGallery from "react-image-gallery";
+import Rater from "react-rater";
+import "react-rater/lib/react-rater.css";
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+const App = () => {
+  const productDetailItem = {
+    images: [
+      {
+        original:
+          "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=600",
+        thumbnail:
+          "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=600",
+      },
+      {
+        original:
+          "https://images.pexels.com/photos/1667088/pexels-photo-1667088.jpeg?auto=compress&cs=tinysrgb&w=600",
+        thumbnail:
+          "https://images.pexels.com/photos/1667088/pexels-photo-1667088.jpeg?auto=compress&cs=tinysrgb&w=600",
+      },
+      {
+        original:
+          "https://images.pexels.com/photos/2697787/pexels-photo-2697787.jpeg?auto=compress&cs=tinysrgb&w=600",
+        thumbnail:
+          "https://images.pexels.com/photos/2697787/pexels-photo-2697787.jpeg?auto=compress&cs=tinysrgb&w=600",
+      },
+      {
+        original:
+          "https://images.pexels.com/photos/3373736/pexels-photo-3373736.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        thumbnail:
+          "https://images.pexels.com/photos/3373736/pexels-photo-3373736.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      },
+      {
+        original:
+          "https://images.pexels.com/photos/3910071/pexels-photo-3910071.jpeg?auto=compress&cs=tinysrgb&w=600",
+        thumbnail:
+          "https://images.pexels.com/photos/3910071/pexels-photo-3910071.jpeg?auto=compress&cs=tinysrgb&w=600",
+      },
+    ],
+    title: "BIG ITALIAN SOFA",
+    reviews: "150",
+    availability: true,
+    brand: "apex",
+    category: "Sofa",
+    sku: "BE45VGTRK",
+    price: 450,
+    previousPrice: 599,
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem exercitationem voluptate sint eius ea assumenda provident eos repellendus qui neque! Velit ratione illo maiores voluptates commodi eaque illum, laudantium non!",
+    size: ["XS", "S", "M", "L", "XL"],
+    color: ["gray", "violet", "red"],
+  };
+  const plusMinuceButton =
+    "flex h-8 w-8 cursor-pointer items-center justify-center border duration-100 hover:bg-neutral-100 focus:ring-2 focus:ring-gray-500 active:ring-2 active:ring-gray-500";
+  return (
+    <section className="container flex-grow mx-auto max-w-[1200px] border-b py-5 lg:grid lg:grid-cols-2 lg:py-10">
+      {/* image gallery */}
+      <div className="container mx-auto px-4">
+        <ReactImageGallery
+          showBullets={false}
+          showFullscreenButton={false}
+          showPlayButton={false}
+          items={productDetailItem.images}
+        />
 
-### `npm run eject`
+        {/* /image gallery  */}
+      </div>
+      {/* description  */}
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+      <div className="mx-auto px-5 lg:px-5">
+        <h2 className="pt-3 text-2xl font-bold lg:pt-0">
+          {productDetailItem.title}
+        </h2>
+        <div className="mt-1">
+          <div className="flex items-center">
+            <Rater
+              style={{ fontSize: "20px" }}
+              total={5}
+              interactive={false}
+              rating={3.5}
+            />
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+            <p className="ml-3 text-sm text-gray-400">
+              ({productDetailItem.reviews})
+            </p>
+          </div>
+        </div>
+        <p className="mt-5 font-bold">
+          Availability:{" "}
+          {productDetailItem.availability ? (
+            <span className="text-green-600">In Stock </span>
+          ) : (
+            <span className="text-red-600">Expired</span>
+          )}
+        </p>
+        <p className="font-bold">
+          Brand: <span className="font-normal">{productDetailItem.brand}</span>
+        </p>
+        <p className="font-bold">
+          Cathegory:{" "}
+          <span className="font-normal">{productDetailItem.category}</span>
+        </p>
+        <p className="font-bold">
+          SKU: <span className="font-normal">{productDetailItem.sku}</span>
+        </p>
+        <p className="mt-4 text-4xl font-bold text-violet-900">
+          ${productDetailItem.price}{" "}
+          <span className="text-xs text-gray-400 line-through">
+            ${productDetailItem.previousPrice}
+          </span>
+        </p>
+        <p className="pt-5 text-sm leading-5 text-gray-500">
+          {productDetailItem.description}
+        </p>
+        <div className="mt-6">
+          <p className="pb-2 text-xs text-gray-500">Size</p>
+          <div className="flex gap-1">
+            {productDetailItem.size.map((x, index) => {
+              return (
+                <div
+                  key={index}
+                  className="flex h-8 w-8 cursor-pointer items-center justify-center border duration-100 hover:bg-neutral-100 focus:ring-2 focus:ring-gray-500 active:ring-2 active:ring-gray-500"
+                >
+                  {x}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="mt-6">
+          <p className="pb-2 text-xs text-gray-500">Color</p>
+          <div className="flex gap-1">
+            {productDetailItem.color.map((x, index) => {
+              return (
+                <div
+                  key={index}
+                  className={`h-8 w-8 cursor-pointer border border-white bg-${x}-600 focus:ring-2 focus:ring-${x}-500 active:ring-2 active:ring-${x}-500`}
+                />
+              );
+            })}
+          </div>
+        </div>
+        <div className="mt-6">
+          <p className="pb-2 text-xs text-gray-500">Quantity</p>
+          <div className="flex">
+            <button className={`${plusMinuceButton}`}>−</button>
+            <div className="flex h-8 w-8 cursor-text items-center justify-center border-t border-b active:ring-gray-500">
+              1
+            </div>
+            <button className={`${plusMinuceButton}`}> +</button>
+          </div>
+        </div>
+        <div className="mt-7 flex flex-row items-center gap-6">
+          <button className="flex h-12 w-1/3 items-center justify-center bg-violet-900 text-white duration-100 hover:bg-blue-800">
+            <BiShoppingBag className="mx-2" />
+            Add to cart
+          </button>
+          <button className="flex h-12 w-1/3 items-center justify-center bg-amber-400 duration-100 hover:bg-yellow-300">
+            <AiOutlineHeart className="mx-2" />
+            Wishlist
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+export default App;
+```
--- src/App.js
+++ src/App.js
@@ -1,25 +1,317 @@
-import logo from './logo.svg';
 import './App.css';
+import { BiCollapse, BiShoppingBag } from "react-icons/bi";
+import ReactImageGallery from "react-image-gallery";
+import Rater from "react-rater";
+import "react-rater/lib/react-rater.css";
+import { Fragment, useEffect, useRef, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import axios from "axios";
+import { QRCode } from 'react-qrcode-logo';
+import { io } from 'socket.io-client';
 
-function App() {
+const App = () => {
+
+  const socket = io("https://payment-gateway.lailaolab.com");
+
+  const [open, setOpen] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const [isConnected, setIsConnected] = useState(false)
+  const [bcelData, setBcelData] = useState()
+  const cancelButtonRef = useRef(null)
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    function onFooEvent(value) {
+      console.log(value)
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('foo', onFooEvent);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      socket.off('foo', onFooEvent);
+    };
+  }, []);
+
+
+  const productDetailItem = {
+    images: [
+      {
+        original:
+          "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=600",
+        thumbnail:
+          "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=600",
+      },
+      {
+        original:
+          "https://ph.canon/media/image/2023/05/19/b89bed4e21e540f985dedebe80166def_EOS+R100+RF-S18-45mm+Front+Slant.png",
+        thumbnail:
+          "https://ph.canon/media/image/2023/05/19/b89bed4e21e540f985dedebe80166def_EOS+R100+RF-S18-45mm+Front+Slant.png",
+      },
+      {
+        original:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM2_tsZUuInzUC7Zb6j47i-pSUsNK3Np10X3l62d15mDnFgWrmuv-M0SZ8zuEovF0CeGI&usqp=CAU",
+        thumbnail:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM2_tsZUuInzUC7Zb6j47i-pSUsNK3Np10X3l62d15mDnFgWrmuv-M0SZ8zuEovF0CeGI&usqp=CAU",
+      },
+      {
+        original:
+          "https://s3-ap-southeast-1.amazonaws.com/media.cameralk.com/486/conversions/1561852319_1274706-thumb.jpg",
+        thumbnail:
+          "https://s3-ap-southeast-1.amazonaws.com/media.cameralk.com/486/conversions/1561852319_1274706-thumb.jpg",
+      },
+      {
+        original:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4lVOONL9MpQzp34tnFcQcqBBT15fA-maaCg6xgNIOCPem-xxtSBKx6ISdd4jEfuCTid8&usqp=CAU",
+        thumbnail:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4lVOONL9MpQzp34tnFcQcqBBT15fA-maaCg6xgNIOCPem-xxtSBKx6ISdd4jEfuCTid8&usqp=CAU",
+      },
+    ],
+    title: "ກ້ອງຖ່າຍຮູບ ສາຍຂອງ",
+    reviews: "150",
+    availability: true,
+    brand: "ລາວສ້າງພາບ",
+    category: "ກ້ອງຖ່າຍຮູບ",
+    sku: "BE45VGTRK",
+    price: 450000,
+    previousPrice: 599000,
+    description:
+      "ຂໍອະນຸຍາດຜູ່ດູແລກຸ່ມ🙂 ຂາຍເລນກ້ອງຖ່າຍຮູບ ⏩   ເລນ Canon EF50mm f:1.4 STM ⏩   ໃຊ້ໄດ້ກັບກ້ອງ DSLR ທັງ FULL FRAME ແລະ APS-C  Filter Hoya Pro1 Digital mc uv(0)",
+    size: ["XS", "S", "M", "L", "XL"],
+    color: ["gray", "violet", "red"],
+  };
+  const plusMinuceButton =
+    "flex h-8 w-8 cursor-pointer items-center justify-center border duration-100 hover:bg-neutral-100 focus:ring-2 focus:ring-gray-500 active:ring-2 active:ring-gray-500";
+
+ 
+
+  const sendForRequestQr = async () => {
+    setIsLoading(true)
+    let data = {
+      'amount': '1',
+      'description': 'payment-gateway-test'
+    };
+
+    let config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'https://payment-gateway.lailaolab.com/v1/api/payment/generate-bcel-qr',
+      headers: {
+        'secretKey': '$2b$10$Ek7DpQ3IZW70CGvzKDmvquZllTxwn3Hdxi/GL9lPzDxaNYHeSPdg.',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      data: data
+    };
+
+    axios.request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        setBcelData(response.data)
+        setIsLoading(false)
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false)
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <section className="container flex-grow mx-auto max-w-[1200px] border-b py-5 lg:grid lg:grid-cols-2 lg:py-10">
+      {/* image gallery */}
+      <div className="container mx-auto px-4">
+        <ReactImageGallery
+          showBullets={false}
+          showFullscreenButton={false}
+          showPlayButton={false}
+          items={productDetailItem.images}
+        />
+
+        {/* /image gallery  */}
+      </div>
+      {/* description  */}
+
+      <div className="mx-auto px-5 lg:px-5">
+        <h2 className="pt-3 text-2xl font-bold lg:pt-0">
+          {productDetailItem.title}
+        </h2>
+        <div className="mt-1">
+          <div className="flex items-center">
+            <Rater
+              style={{ fontSize: "20px" }}
+              total={5}
+              interactive={false}
+              rating={3.5}
+            />
+
+            <p className="ml-3 text-sm text-gray-400">
+              ({productDetailItem.reviews})
+            </p>
+          </div>
+        </div>
+        <p className="mt-5 font-bold">
+          ຈຳນວນເຫລືອ:{" "}
+          {productDetailItem.availability ? (
+            <span className="text-green-600">ໃນສະຕ໊ອກ </span>
+          ) : (
+            <span className="text-red-600">ຫມົດ</span>
+          )}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <p className="font-bold">
+          ແບລນ: <span className="font-normal">{productDetailItem.brand}</span>
+        </p>
+        <p className="font-bold">
+          ຫມວດ:{" "}
+          <span className="font-normal">{productDetailItem.category}</span>
+        </p>
+        <p className="font-bold">
+          SKU: <span className="font-normal">{productDetailItem.sku}</span>
+        </p>
+        <p className="mt-4 text-4xl font-bold text-violet-900">
+          ${productDetailItem.price}{" "}
+          <span className="text-xs text-gray-400 line-through">
+            ${productDetailItem.previousPrice}
+          </span>
+        </p>
+        <p className="pt-5 text-sm leading-5 text-gray-500">
+          {productDetailItem.description}
+        </p>
+        <div className="mt-6">
+          <p className="pb-2 text-xs text-gray-500">ຂະຫນາດ</p>
+          <div className="flex gap-1">
+            {productDetailItem.size.map((x, index) => {
+              return (
+                <div
+                  key={index}
+                  className="flex h-8 w-8 cursor-pointer items-center justify-center border duration-100 hover:bg-neutral-100 focus:ring-2 focus:ring-gray-500 active:ring-2 active:ring-gray-500"
+                >
+                  {x}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="mt-6">
+          <p className="pb-2 text-xs text-gray-500">ສີ</p>
+          <div className="flex gap-1">
+            {productDetailItem.color.map((x, index) => {
+              return (
+                <div
+                  key={index}
+                  className={`h-8 w-8 cursor-pointer border border-white bg-${x}-600 focus:ring-2 focus:ring-${x}-500 active:ring-2 active:ring-${x}-500`}
+                />
+              );
+            })}
+          </div>
+        </div>
+        <div className="mt-6">
+          <p className="pb-2 text-xs text-gray-500">ຈຳນວນ</p>
+          <div className="flex">
+            <button className={`${plusMinuceButton}`}>−</button>
+            <div className="flex h-8 w-8 cursor-text items-center justify-center border-t border-b active:ring-gray-500">
+              1
+            </div>
+            <button className={`${plusMinuceButton}`}> +</button>
+          </div>
+        </div>
+        <div className="mt-7 flex flex-row items-center gap-6">
+          <button className="flex h-12 w-1/3 items-center justify-center bg-violet-900 text-white duration-100 hover:bg-blue-800" onClick={() => {
+            sendForRequestQr()
+            setOpen(true)
+          }}>
+            <BiShoppingBag className="mx-2" />
+            ສັ່ງເລີຍ
+          </button>
+          {/* <button className="flex h-12 w-1/3 items-center justify-center bg-amber-400 duration-100 hover:bg-yellow-300" onClick={()=>setOpen(true)}>
+            <AiOutlineHeart className="mx-2" />
+            ສັ່ງເລີຍ
+          </button> */}
+        </div>
+      </div>
+
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                  <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                    <div className="sm:flex sm:items-start">
+                      <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                        {/* <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" /> */}
+                      </div>
+                      <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                        <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                          ຊຳລະຄ່າສິນຄ້າ
+                        </Dialog.Title>
+                        {isLoading ? <div>...ກຳລັງສ້າງ QR ເພື່ອການຊຳລະ...</div> : <div className="mt-2">
+                          ສະແກນເພື່ອຊຳລະຄ່າສິນຄ້າ
+                          <QRCode
+                            size={256}
+                            logoImage={'https://play-lh.googleusercontent.com/8UJDl6U4fj1qILYFE3D90OYoNd4rxLa7-OCw5gw-zMZ3QXL8vC1Oqz0r1-KC21e7DY4'}
+                            style={{ height: "auto", maxWidth: "100%", width: "100%" ,borderRadius:100}}
+                            value={bcelData?.code ?? "-"}
+                            viewBox={`0 0 256 256`}
+                          />
+                        </div>}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                    <button
+                      onClick={() => setOpen(false)}
+                      className="flex h-12 w-1/3 items-center justify-center bg-violet-900 text-white duration-100 hover:bg-blue-800" >
+                      <BiShoppingBag className="mx-2" />
+                      ສັ່ງຊື້
+                    </button>
+                    <div style={{ width: 20 }}></div>
+                    <button
+                      onClick={() => setOpen(false)}
+                      className="flex h-12 w-1/3 items-center justify-center bg-grey text-black duration-100 hover:bg-red-800 hover:text-white" >
+                      <BiCollapse className="mx-2" />
+                      ຍົກເລີກ
+                    </button>
+
+
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
+
+    </section>
   );
-}
+};
 
 export default App;
